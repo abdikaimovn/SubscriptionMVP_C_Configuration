df71c2eaab6cffc5fbf826cbc41e68034d5501e8
Nurdaulet <56686500+abdikaimovn@users.noreply.github.com>
2024-12-27 12:00:11 +0500

    init

---
+++ Subscription_MVP_C_Configuration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subscription_MVP_C_Configuration
+//
+//  Created by Нурдаулет on 27.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
